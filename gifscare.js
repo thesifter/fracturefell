4df2f28dc92245c3a6b15d41ba1c7eeb5ed95475
Damen Hansen <damen.hansen@gmail.com>
2025-04-05 20:19:23 +1300

    Update gifscare.js

--- gifscare.js
+++ gifscare.js
@@ -15,7 +15,7 @@
 
   function showMindFlash() {
     const flash = document.createElement('img');
-    flash.src = window.location.origin + 'images/fracturefell_flash.gif;
+    flash.src = window.location.origin + '/images/fracturefell_flash.gif;
     flash.alt = 'fracture glimpse';
     flash.style.position = 'fixed';
     flash.style.top = '50%';e
