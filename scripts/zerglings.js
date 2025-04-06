ceea2a62c076c77a912d7af32c00e3d27cca732d
Damen Hansen <damen.hansen@gmail.com>
2025-04-06 21:03:37 +1200

    Update zerglings.js

--- scripts/zerglings.js
+++ scripts/zerglings.js
@@ -3,9 +3,8 @@
 window.FractureFell = window.FractureFell || {};
 
 export function releaseZergling() {
-
-
   const id = `zerg-${Date.now()}`;
+  window.FractureFell = window.FractureFell || {};
   window.FractureFell.lastZerg = {
     id,
     timestamp: Date.now()
@@ -13,7 +12,11 @@ export function releaseZergling() {
   console.log(`[zerglings] Releasing ${id}`);
 
   const zerg = document.createElement('img');
-  zerg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAAASCAYAAABuQKz0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABt0lEQVRoge2WQU7DQAyGG3//p5iLGDjaXTqKbVdD1WmDLp8g6fsCa5X6CHbhvNSzOSBIx4CyIjUMqAwQZAxQSAZYZ+qFt4XO/cFvbg54mlyPVrS9aNzBi15VaEZF0QvQFZKV+nT5GRm54RBg1HdFhZWlxogmExLZqd2HVbPzH3KUSmYOskKJt9SMIRFeRgG8jvPXiVz7KM4cySD2BoDukHzc6NpaEv7YymmgZCRBvYv5MLZG5NRuhMhsZ5pnp1v50NUCYaPlc1w+GyZ7JcSwWrUopVdEaHkHUpZT1RaxEa07MkZ4XLKo3/5jR8/3g03hZmRXABDWjIatG8W8+RJAKfYmp8JFxrwqRAPfJSzJrIqjWNHtS3D+yptAY+5d34S6PNHeuuhgfNj6ntEvVSTZ2XFLZxyKM+SyZFMKvY7w99GLx4JACiUbqAAAAAElFTkSuQmCC';
+
+  // Use the properly formatted base64 string here
+  const spriteSrc = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAAASCAYAAABuQKz0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABt0lEQVRoge2WQU7DQAyGG3//p5iLGDjaXTqKbVdD1WmDLp8g6fsCa5X6CHbhvNSzOSBIx4CyIjUMqAwQZAxQSAZYZ+qFt4XO/cFvbg54mlyPVrS9aNzBi15VaEZF0QvQFZKV+nT5GRm54RBg1HdFhZWlxogmExLZqd2HVbPzH3KUSmYOskKJt9SMIRFeRgG8jvPXiVz7KM4cySD2BoDukHzc6NpaEv7YymmgZCRBvYv5MLZG5NRuhMhsZ5pnp1v50NUCYaPlc1w+GyZ7JcSwWrUopVdEaHkHUpZT1RaxEa07MkZ4XLKo3/5jR8/3g03hZmRXABDWjIatG8W8+RJAKfYmp8JFxrwqRAPfJSzJrIqjWNHtS3D+yptAY+5d34S6PNHeuuhgfNj6ntEvVSTZ2XFLZxyKM+SyZFMKvY7w99GLx4JACiUbqAAAAAElFTkSuQmCC';
+
+  zerg.src = spriteSrc;
   zerg.classList.add('zergling');
 
   Object.assign(zerg.style, {
@@ -36,3 +39,4 @@ export function releaseZergling() {
 
   setTimeout(() => zerg.remove(), 4000);
 }
+
