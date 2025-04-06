c9766b10b23078c72a8e41c7f0fe9bc8c9bd5550
Damen Hansen <damen.hansen@gmail.com>
2025-04-06 20:58:34 +1200

    Update zerglings.js zerg img

--- scripts/zerglings.js
+++ scripts/zerglings.js
@@ -3,6 +3,8 @@
 window.FractureFell = window.FractureFell || {};
 
 export function releaseZergling() {
+const zerg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAAASCAYAAABuQKz0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABt0lEQVRoge2WQU7DQAyGG3//p5iLGDjaXTqKbVdD1WmDLp8g6fsCa5X6CHbhvNSzOSBIx4CyIjUMqAwQZAxQSAZYZ+qFt4XO/cFvbg54mlyPVrS9aNzBi15VaEZF0QvQFZKV+nT5GRm54RBg1HdFhZWlxogmExLZqd2HVbPzH3KUSmYOskKJt9SMIRFeRgG8jvPXiVz7KM4cySD2BoDukHzc6NpaEv7YymmgZCRBvYv5MLZG5NRuhMhsZ5pnp1v50NUCYaPlc1w+GyZ7JcSwWrUopVdEaHkHUpZT1RaxEa07MkZ4XLKo3/5jR8/3g03hZmRXABDWjIatG8W8+RJAKfYmp8JFxrwqRAPfJSzJrIqjWNHtS3D+yptAY+5d34S6PNHeuuhgfNj6ntEvVSTZ2XFLZxyKM+SyZFMKvY7w99GLx4JACiUbqAAAAAElFTkSuQmCC';
+
   const id = `zerg-${Date.now()}`;
   window.FractureFell.lastZerg = {
     id,
